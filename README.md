c64bea98ad5e9325c358d2d19b28d86eea31f4fa
Andy Green <andygrn@gmail.com>
2011-08-08 20:25:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Roulette Generator
+
+Another little geometric toy to generate some cool shapes. It uses the same principles as that old favourite, Spirograph.
+
+## Options
+
+A **Hypotrochoid** is a little cog running inside a bigger cog. An **Epitrochoid** is a little cog running outside a bigger cog.
+
+**Sharpness** is pretty straightforward. You'll probably want to keep it above 60, but you can get cool effects with very low values.
+
+**Inner Radius** is the radius of the big cog.
+
+**Outer Radius** is the radius of the little cog.
+
+**Distance** is the distance from the center of the little cog to the 'pen'. Unlike real Spirograph, it can be more than the radius of the little cog.
+
+## I couldn't have done this without:
+
+[Mozilla.org Canvas Tutorials](https://developer.mozilla.org/en/HTML/Canvas)  
+[Wikipedia Hypotrochoid](http://en.wikipedia.org/wiki/Hypotrochoid)  
+[Wikipedia Epitrochoid](http://en.wikipedia.org/wiki/Epitrochoid)
---
+++ roulettegen.js
@@ -0,0 +1,249 @@
+﻿
+/*
+
+	Roulette Generator
+	Andy Green
+	http://andygrn.co.uk
+	August 2011
+
+*/
+
+var canvas,
+	pen,
+	parameter_inputs = [],
+	colour_inputs = [];
+
+function init()
+{
+
+	var detect_feature = document.createElement('input');
+	detect_feature.setAttribute('type', 'range');
+	
+	if (detect_feature.type !== 'range')
+	{
+		
+		document.getElementById('compatibility-message').style.display = 'block';
+		
+	}
+	
+	canvas = document.getElementById('canvas');
+	
+	pen = canvas.getContext('2d');
+	pen.translate(canvas.width / 2, canvas.height / 2);
+	
+	parameter_input_ids = [
+		'param-spiral-sharpness',
+		'param-spiral-inner-radius',
+		'param-spiral-outer-radius',
+		'param-spiral-distance',
+		'param-line-thickness'
+	];
+	
+	colour_input_ids = [
+		'param-line-colour-r',
+		'param-line-colour-g',
+		'param-line-colour-b'
+	];
+	
+	for (var i in parameter_input_ids)
+	{
+	
+		parameter_inputs[i] = document.getElementById(parameter_input_ids[i]);
+	
+	}
+	
+	for (var i in colour_input_ids)
+	{
+	
+		colour_inputs[i] = document.getElementById(colour_input_ids[i]);
+	
+	}
+	
+	initUI();
+	prepareDraw();
+
+}
+
+function initUI()
+{
+
+	document.getElementById('control-draw').addEventListener('click', prepareDraw, false);
+	document.getElementById('control-save').addEventListener('click', saveCanvas, false);
+	document.getElementById('control-clear').addEventListener('click', clearCanvas, false);
+
+	for (var i in parameter_inputs)
+	{
+
+		document.getElementById(parameter_inputs[i].id + '-value').innerHTML = parameter_inputs[i].value;
+
+		parameter_inputs[i].addEventListener('input', function(){
+
+			var readout = document.getElementById(this.id + '-value')
+			readout.innerHTML = this.value;
+			
+		}, false);
+		
+	}
+
+}
+
+function prepareDraw()
+{
+
+	var values = [],
+		colour = [],
+		roulette_option = document.getElementById('option-roulette-type1').value;
+	
+	for (var i in parameter_inputs)
+	{
+		
+		values[i] = parseInt(parameter_inputs[i].value);
+		
+	}
+	
+	for (var i in colour_inputs)
+	{
+		
+		var c = parseInt(colour_inputs[i].value);
+		
+		if (c > 255)
+		{
+		
+			c = 255;
+		
+		}
+		else if (c < 0)
+		{
+		
+			c = 0;
+		
+		}
+		
+		colour[i] = c;
+		
+	}
+	
+	if (roulette_option === 'on')
+	{
+	
+		drawEpitrochoid(
+			values[0],
+			values[1],
+			values[2],
+			values[3],
+			values[4],
+			colour
+		);
+	
+	}
+	else
+	{
+	
+		drawHypotrochoid(
+			values[0],
+			values[1],
+			values[2],
+			values[3],
+			values[4],
+			colour
+		);
+	
+	}
+
+}
+
+function drawEpitrochoid(sharpness, inner_radius, outer_radius, distance, thickness, colour)
+{
+
+	var	t = 0,
+		xi = inner_radius + outer_radius + distance,
+		yi = 0,
+		x,
+		y;
+	
+	pen.lineWidth = thickness;
+	pen.strokeStyle = 'rgba(' + colour[0] + ', ' + colour[1] + ', ' + colour[2] + ', 1)';
+	
+	pen.beginPath();
+	pen.moveTo(xi, yi);
+	
+	do
+	{
+		
+		if (t > 10000)
+		{
+		
+			break;
+		
+		}
+
+		t += Math.PI / sharpness;
+		
+		x = ((inner_radius + outer_radius) * Math.cos(t)) + (distance * Math.cos(((inner_radius + outer_radius) / outer_radius) * (t)));
+		y = ((inner_radius + outer_radius) * Math.sin(t)) - (distance * Math.sin(((inner_radius + outer_radius) / outer_radius) * (t)));
+		
+		pen.lineTo(x, y);
+		
+	}
+	while (x != xi && y != yi)
+	
+	pen.stroke();
+
+}
+
+function drawHypotrochoid(sharpness, inner_radius, outer_radius, distance, thickness, colour)
+{
+
+	var	t = 0,
+		xi = (inner_radius - outer_radius) + distance,
+		yi = 0,
+		x,
+		y;
+	
+	pen.lineWidth = thickness;
+	pen.strokeStyle = 'rgba(' + colour[0] + ', ' + colour[1] + ', ' + colour[2] + ', 1)';
+	
+	pen.beginPath();
+	pen.moveTo(xi, yi);
+	
+	do
+	{
+		
+		if (t > 10000)
+		{
+		
+			break;
+		
+		}
+		
+		t += Math.PI / sharpness;
+		
+		x = ((inner_radius - outer_radius) * Math.cos(t)) + (distance * Math.cos(((inner_radius - outer_radius) / outer_radius) * (t)));
+		y = ((inner_radius - outer_radius) * Math.sin(t)) - (distance * Math.sin(((inner_radius - outer_radius) / outer_radius) * (t)));
+
+		pen.lineTo(x, y);
+		
+	}
+	while (x != xi && y != yi)
+	
+	pen.stroke();
+
+}
+
+function clearCanvas()
+{
+
+	pen.clearRect(canvas.width / -2, canvas.height / -2, canvas.width, canvas.height);
+
+}
+
+function saveCanvas()
+{
+
+	var image_data = canvas.toDataURL('image/png');
+
+//	image_data = image_data.replace('image/png', 'application/octet-stream');
+
+	document.location.href = image_data;
+
+}
